35700122d9c5bba3ec506c648d1c74f1bbb7da7a
Steve Clark <steve.a.clark48@gmail.com>
2021-08-11 15:11:33 -0500

    footer
    Created Footer files

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+      <footer className='footer'>
+          <h4>Copyright @ 2021</h4>
+      </footer>
+  );
+}
+ 
+export default Footer;
